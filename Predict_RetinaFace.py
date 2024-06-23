ae20abfa029a5b10de0f281ba644b1294327d696
aattyy-102030 <yutam1519@gmail.com>
2024-06-23 23:38:21 +0900

    検証用に更新

--- Predict_RetinaFace.py
+++ Predict_RetinaFace.py
@@ -1,6 +1,6 @@
 from retinaface import RetinaFace
 import torch
-import albumentations as A
+# import albumentations as A
 import cv2
 
 from Create_LearnedModel import face_detection
@@ -10,20 +10,20 @@ os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 class predict_retinaface():
 	def __init__(self) -> None:
 		self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-		self.retinaface_model = RetinaFace(device = self.device)
+		self.retinaface_model = RetinaFace
 		# self.sample_img = "Sample_Image.jpg"
-		self.jpg = "jpg".casefold()
-		self.png = "png".casefold()
-		self.sample_imgs = [path for ext in [self.jpg, self.png] for path in glob.glob('Sample_Image/*.{}'.format(ext))]
+		self.files = os.listdir("./Sample_Image")
+		self.sample_imgs = [os.path.join("./Sample_Image", file) for file in self.files if file.lower().endswith(('.jpg', '.png'))]
 		self.now_str = datetime.datetime.now()
 
 	def check_output_dir(self):
-		instance = face_detection()
-		self.learned_model_path = instance.main()
+		# instance = face_detection()
+		# self.learned_model_path = instance.main()
+		self.learned_model_path = "model_20240613-2154.pth"
 		print(f"Output model path : {self.learned_model_path}")
 
-	def detect_faces(self) -> dict:
-		faces = self.retinaface_model.detect_faces()
+	def detect_faces(self, img) -> dict:
+		faces = self.retinaface_model.detect_faces(img)
 		return faces
 
 	def load_learned_model(self):
@@ -58,7 +58,7 @@ class predict_retinaface():
 		print("INFO: START prediction by RetinaFace. ")
 		for i, img in enumerate(imgs,start=1):
 			self.check_output_dir()
-			faces = self.detect_faces()
+			faces = self.detect_faces(img)
 			learned_model = self.load_learned_model()
 			result_image = self.annotate_faces(faces, img, learned_model)
 			cv2.imwrite(f'Sample_result_'+ i +'.jpg', result_image)
